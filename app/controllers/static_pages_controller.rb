ab005c16f660272cf9316d54c4e0f4eab78cdeb0
John Moran <cyncastic@gmail.com>
2022-03-02 12:17:38 -0500

    another change

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
 
 	def home
     	@brands = Brand.all
+    	@product_types = ProductType.all
+    	@note_categories = NoteCategory.all
 	end
 
 end
